9ba21efcfd94f0962f1a2e03fc43cd829be82771
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-09-11 20:02:22 +0530

    Update and rename vps_config.py to config.py

--- config.py
+++ config.py
@@ -5,30 +5,25 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 92729829
-    API_HASH = ""
+    APP_ID = 25742938
+    API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
     # the name to display in your alive message
     ALIVE_NAME = "♡_🫧𝆺꯭𝅥˶‌‌֟፝★Ｅ𝓪𝘨ļ૯⁂★🍷┼❤️༆"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = ""
+    DB_URI = "postgres://lwktrpsc:1UlTfWHhnxKZWRETlLu6AF0x4AsyKXhO@rosie.db.elephantsql.com/lwktrpsc"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
-    LEGEND_STRING = ""
+    EAGLE_STRING = "1BVtsOHwBu1VHBhQ2hSL7xirg97PH43PUjAHTO679WxWL9mhbuUGaZusXJ_qFTYdvuce_EWVJj4_WtvRvMJ60Yh1h9f27rU91nrkjn4Yc5dt6BzCJkrjtxGo7ryPeG6ttTJfDkl0c1uD9NTLPIo2aaGrBDW-hJJ9dF1QTu1E1iGYkp3EGX1y1r84k-nrvkH4FSk_VUUyXhJttHqg8WRO2X1b33NxfEFfnKwgRPx1doq-yxCDMkyrpMFePFasikyYI0fqWQ8QiVQ2WgsiK8QZlJLGeJBuxZgaOSfZd3FMeL28UxGsLACG11CXtBVy5GtqwXWqdeGRefyDGZhx61h8zKB_KjaLeVeA="
     # create a new bot in @botfather and fill the following vales with bottoken
-    BOT_TOKEN = ""
+    BOT_TOKEN = "7359812372:AAEMrQGAySNHtfnYMHE52Q8PVQpt7JprvTc"
     # command handler
     HANDLER = "."
     # command hanler for sudo
     SUDO_HANDLER = "."
-    # External plugins repo
-    EXTERNAL_REPO = True
-    EXTERNAL_REPOBRANCH = "Bad"
     UPSTREAM_REPO = "Bad"
     VCMODE = False
     # Your City's TimeZone
     TZ = "Asia/Kolkata"
-    # Random stuff Key
-    RANDOM_STUFF_API_KEY = ""
-    # your logger group Id
-    PRIVATE_GROUP_BOT_API_ID = 
+    RANDOM_STUFF_API_KEY = "20240908-1401-4740-801f-0d56de118901"
+    PRIVATE_GROUP_BOT_API_ID = -1002342412856
     # owner id
     OWNER_ID = 5144246533
