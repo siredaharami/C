3496cd04fc480d75eee379471155ce24a4f45781
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-09-11 22:10:50 +0530

    Update decorators.py

--- Eaglebot/core/decorators.py
+++ Eaglebot/core/decorators.py
@@ -3,14 +3,14 @@ import asyncio
 from telethon.errors import FloodWaitError, MessageNotModifiedError
 from telethon.events import CallbackQuery
 
-from ..Config import config
+from ..Config import Config
 from ..sql_helper.globals import gvarstatus
 
 
 def check_owner(func):
     async def wrapper(c_q: CallbackQuery):
         if c_q.query.user_id and (
-            c_q.query.user_id == config.OWNER_ID
+            c_q.query.user_id == Config.OWNER_ID
             or c_q.query.user_id in Config.SUDO_USERS
         ):
             try:
