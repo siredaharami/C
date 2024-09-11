72824244863b3db812764da6c2a0ee9e10782119
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-09-11 23:26:36 +0530

    Update inlinebot.py

--- Eaglebot/core/inlinebot.py
+++ Eaglebot/core/inlinebot.py
@@ -74,8 +74,11 @@ async def on_plug_in_callback_query_handler(event):
         ),
         (Button.inline(f"👨‍💻 Main Menu", data="mainmenu"),),
     ]
-
-await event.edit(text, buttons=buttons)
+    await event.edit(
+        f"🔱『{mention}』🔱",
+        buttons=buttons,
+        link_preview=False,
+)
 
 
 def main_menu():
