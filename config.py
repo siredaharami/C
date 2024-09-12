21d8e9379657d964c888f2b71f09cb8ad1fc0c30
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-09-12 21:47:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://lwktrpsc:1UlTfWHhnxKZWRETlLu6AF0x4AsyKXhO@rosie.db.elephantsql.com/lwktrpsc"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
-    EAGLE_STRING = "1BVtsOJ4Bu4ay-3wY0vB7HjfkFzpFN6s8sxx6ybTGr1iZGSZeq7MXsCblxV-_CXVXMv88KEfQWqFOf0ptMwQYJ0CpGon5Eg2KaQwMNliurLdSZIqU3g3tj6QfPxsHeIHd6UMdwXvJsBqLNH7kmiXEwHPvozoXMES3ELqQ4uTdH_WsylTu0gZX8i9hKvIdJRp1NhhJleQes-sJoWLLp4uCY3wFv7hrKwjuRTPmwXvSHr9fw7ixYPp72ikVsASos6qIoVZ4OgMM2Yfl2Pn40-ST8ywiV7U5MJpokoCoE8NbV-8CthIDxCOPPc0nqPOUfzVzDB-tRKJpYRefNy2q8y9ny-twBfAbUro="
+    EAGLE_STRING = "1BVtsOJ4Buy0RTOdwFs4YmeMz84LW8KkzSmJ0E4nf9U3e_kiltZUkOp3SwhiSz5SMi1Vg9SO13ZcSYfTHcFhQ1JCINK6KniEIwKHVWBIxFE56D1aUugIMiJytScOQ72kk9alnRjpsk1jmoNzz-j5uw9O9l49MFyNCD_pLp6EUJVDAqTmSbbDDRMm3UMjpAXKCjoHM3GJ7QBJMKqPGOFnUmTh7Tt6YM7VpieiYjopDp8fL0byARtms3rZ0P1skbs87fDqWFMxjfqo8fXqAgZhZFWoBcOfhEAIXyVOf0OhYL-DZ-y2MxYscC9D6-ptokxYtb00zwwJPphb3cjlZ-3xr0_s6kLVxlnM="
     # create a new bot in @botfather and fill the following vales with bottoken
     BOT_TOKEN = "7359812372:AAEMrQGAySNHtfnYMHE52Q8PVQpt7JprvTc"
     # command handler
