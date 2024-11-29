fd139ddcd332b6d1baaba272b2bd273914bb7d1e
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-29 18:24:05 +0530

    Update __init__.py

--- Eaglebot/helpers/utils/__init__.py
+++ Eaglebot/helpers/utils/__init__.py
@@ -3,6 +3,7 @@ from .paste import *
 
 type = True
 check = 0
+
 while type:
     try:
         from . import format as _format
@@ -15,7 +16,13 @@ while type:
 
         break
     except ModuleNotFoundError as e:
+        def install_pip(package_name):
+            import subprocess
+            import sys
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+
         install_pip(e.name)
         check += 1
         if check > 5:
+            print("Module installation failed after multiple attempts.")
             break
