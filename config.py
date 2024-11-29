cbad5ae13270987aa4b87d089c14fc520b6d5a7e
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-29 10:03:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "♡_🫧𝆺꯭𝅥˶‌‌֟፝★Ｅ𝓪𝘨ļ૯⁂★🍷┼❤️༆"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://lwktrpsc:1UlTfWHhnxKZWRETlLu6AF0x4AsyKXhO@rosie.db.elephantsql.com/lwktrpsc"
+    DB_URI = "postgres://oaykvtmj:bsIGPV7wmId1x1CNH9eqxQVX5t25cHI3@manny.db.elephantsql.com/oaykvtmj"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
-    EAGLE_STRING = "1BVtsOJMBu0bNyl8UyBdsMSOziHn6kJAQfG7OOtxmPgirlyGAH5yiLbtDXKsuZ2El71TuIf86pwTWh0o8JMtemcBMlQkcWP9nN0ymDmkfTMBP17kdWkKVgX8-YjHH9b1NKowYLpC-ivWgMsTgZtvIlkFrfDidQUqZunim9nzzn9aL4P2SsniJxeaaBIDbBYqlt6P5W2vKeQuz0n1FiiYKcWPL-iUWf903knheptQ3-p9bV8DF97IGMpFVossm-l6fKaisPCuuSpHGdVDW-s5RB1lmehYXg1xtyqttqhuDWdazBiw2H3-Za49WTEOd83OfZsna9Ckib4NdXpVWB0yJEExwnbLhrH8="
+    EAGLE_STRING = "1BZWaqwUAUD43K5aIkXqPBYANp7MOkRB81TplGCA85UrUjc-_CIuB7Cu92H8QudHb5ledoQhn66Hf5FEVpuNV4B7foaGjIOyuzA154wLhf79nFH5nDAr63qVu21TUaeRSQ39IGtmg4mqdd1TPwcDZocG6qnY8uJAsLHDYArvTzblNBJxL4dybniOvYhMaWtHc38Pi6AJdXjrkHM2SATQmaP7psmJQ9xdYKLruPmpH7Jl9FDAzpclUY25NV2YXpO5YhLSNJQOv6tPldqrTS-7R7Xp2EzBxJS4Joodc8NRymg_8HRIYs7n39PvZJwRVt1uwnSoCZ0snrdNOvVLnu_guQWDf0e0Uq2k="
     # create a new bot in @botfather and fill the following vales with bottoken
-    BOT_TOKEN = "7473092771:AAFWw5r8dsHmBkKR99g7ki00NhgNGunl8fs"
+    BOT_TOKEN = "7737579995:AAHzTgISMQhZuEasuOBEm6aTn5mqes-f0aQ"
     # command handler
     HANDLER = "."
     # command hanler for sudo
