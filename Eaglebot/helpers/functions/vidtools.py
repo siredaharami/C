8177c0d364190364f53f2e3263c8eb884e56e698
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-29 18:30:35 +0530

    Update vidtools.py

--- Eaglebot/helpers/functions/vidtools.py
+++ Eaglebot/helpers/functions/vidtools.py
@@ -1,7 +1,7 @@
 import logging
 import random
 
-from moviepy.editor import VideoFileClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
 from PIL import Image, ImageOps
 
 from ...core.logger import logging
